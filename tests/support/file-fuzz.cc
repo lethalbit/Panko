fdd710a8d6396223b606d8681271f33b814f0ac8
Aki Van Ness <aki@lethalbit.net>
2025-02-14 01:26:24 -0800

    tests: added a (kinda pathetic) fuzzer harness for the file discrimination

--- tests/support/file-fuzz.cc
+++ tests/support/file-fuzz.cc
@@ -3,8 +3,23 @@
 
 #include <cstdint>
 #include <cstddef>
+#include <string_view>
+
+#include "panko/support/file.hh"
+#include "panko/support/io/raw_file.hh"
 
 extern "C" int LLVMFuzzerTestOneInput(const std::uint8_t *data, std::size_t size) {
+	/* We can't construct 0-sized `string_view`s */
+	if (size == 0) {
+		return -1;
+	}
+
+	const std::string_view str{reinterpret_cast<const char*>(data), size};
+	auto res{Panko::support::open(str)};
+	if (res.has_value()) {
+		auto file{std::get<Panko::support::io::raw_file_t>(std::move(*res))};
+		file.valid();
+	}
 
 	return 0;
 }
