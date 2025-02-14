ead21ddc51a6424881a3494d80886f8356c19404
Aki Van Ness <aki@lethalbit.net>
2025-02-13 19:42:36 -0800

    tests: support: implemented a *very basic* fuzz harness of the sys module

--- tests/support/sys-fuzz.cc
+++ tests/support/sys-fuzz.cc
@@ -3,8 +3,25 @@
 
 #include <cstdint>
 #include <cstddef>
+#include <string_view>
+
+#include "panko/support/sys.hh"
+
+using Panko::support::sys::expand_user;
+using Panko::support::sys::getenv;
 
 extern "C" int LLVMFuzzerTestOneInput(const std::uint8_t *data, std::size_t size) {
+	/* We can't construct 0-sized `string_view`s */
+	if (size == 0) {
+		return -1;
+	}
+
+	const std::string_view str{reinterpret_cast<const char*>(data), size};
+
+	[[maybe_unused]]
+	auto _{expand_user(str)};
+	[[maybe_unused]]
+	auto _{getenv(str)};
 
 	return 0;
 }
