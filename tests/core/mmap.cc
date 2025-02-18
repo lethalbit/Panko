52f3c4710d7e93138bf3d12a2a7689ee18191546
Aki Van Ness <aki@lethalbit.net>
2025-02-18 02:53:13 -0800

    tests: core: Added stub for the mmap test

---
+++ tests/core/mmap.cc
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* mmap.cc - memory mapped file test harness */
+
+#include <cstring>
+#include <cstdint>
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest.h>
+
+
+#include "panko/core/mmap.hh"
