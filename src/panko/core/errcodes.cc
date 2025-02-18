46279e0b426b980a193cc41bb76de5133a2f4541
Aki Van Ness <aki@lethalbit.net>
2025-02-18 02:45:03 -0800

    panko: core: Added some more error codes and cleaned p the errorcode impl

--- src/panko/core/errcodes.cc
+++ src/panko/core/errcodes.cc
@@ -9,7 +9,7 @@
 namespace Panko::core::error_codes {
 	using namespace std::literals::string_view_literals;
 
-	const std::string_view import_error_message(file_error_t err) noexcept {
+	const std::string_view file_error_message(file_error_t err) noexcept {
 		switch (err) {
 			case file_error_t::MagicReadError: {
 				return "Unable to read file magic"sv;
@@ -23,7 +23,31 @@ namespace Panko::core::error_codes {
 				return "Input exhausted, but did not complete import"sv;
 			} case file_error_t::Unspecified:
 			default: {
-				return "Unspecified import error"sv;
+				return "Unspecified file error"sv;
+			}
+		}
+	}
+
+	const std::string_view strdec_error_message(strdec_error_t err) noexcept {
+		switch (err) {
+			case strdec_error_t::Invalid: {
+				return "String is not valid for the given encoding"sv;
+			} case strdec_error_t::Malformed: {
+				return "String is malformed"sv;
+			} case strdec_error_t::Unspecified:
+			default: {
+				return "Unspecified string decoding error"sv;
+			}
+		}
+	}
+
+	const std::string_view decomp_error_message(decomp_error_t err) noexcept {
+		switch (err) {
+			case decomp_error_t::Invalid: {
+				return "Input buffer is not valid compressed data"sv;
+			} case decomp_error_t::Unspecified:
+			default: {
+				return "Unspecified string decoding error"sv;
 			}
 		}
 	}
--- src/panko/core/errcodes.hh
+++ src/panko/core/errcodes.hh
@@ -12,19 +12,74 @@
 #include "panko/internal/defs.hh"
 
 namespace Panko::core::error_codes {
+	/*! \enum Panko::core::error_codes::file_error_t
+		\brief Possible File I/O errors
+	*/
 	enum struct file_error_t : std::uint16_t {
-		MagicReadError = 0x0000'0000, // Unable to read file magic
-		InvalidMagic   = 0x0000'0001, // Invalid file magic
-		UnknownType    = 0x0000'0002, // Unknown file type
-		ReadError      = 0x0000'0003, // Generic file I/O read Error
-		InputExhausted = 0x0000'0004, // Read stream exhausted but ingestion not complete
+		MagicReadError = 0x0000'0000U, /*!<  Unable to read file magic */
+		InvalidMagic   = 0x0000'0001U, /*!<  Invalid file magic */
+		UnknownType    = 0x0000'0002U, /*!<  Unknown file type */
+		ReadError      = 0x0000'0003U, /*!<  Generic file I/O read Error */
+		InputExhausted = 0x0000'0004U, /*!<  Read stream exhausted but ingestion not complete */
 		/* Ensure that the Unspecified error is always the last one, no matter the type */
 		// NOLINTNEXTLINE(cert-int09-c)
-		Unspecified = std::numeric_limits<std::underlying_type_t<file_error_t>>::max()
+		Unspecified = std::numeric_limits<std::underlying_type_t<file_error_t>>::max() /*!< Unspecified Error */
 	};
 
+	/*! \enum Panko::core::error_codes::strdec_error_t
+		\brief Possible string decoding errors
+	*/
+	enum struct strdec_error_t : std::uint8_t {
+		Invalid   = 0x00U, /*!< The string is invalid. */
+		Malformed = 0x01U, /*!< The string is malformed. (same as Invalid?) */
+		Overrun   = 0x02U, /*!< The string length overruns the buffer */
+		/* Ensure that the Unspecified error is always the last one, no matter the type */
+		// NOLINTNEXTLINE(cert-int09-c)
+		Unspecified = std::numeric_limits<std::underlying_type_t<strdec_error_t>>::max() /*!< Unspecified Error */
+	};
+
+	/*! \enum Panko::core::error_codes::decomp_error_t
+		\brief Possible data decompression errors
+	*/
+	enum struct decomp_error_t : std::uint8_t {
+		Invalid = 0x00U, /*!< The input buffer is invalid. */
+		/* Ensure that the Unspecified error is always the last one, no matter the type */
+		// NOLINTNEXTLINE(cert-int09-c)
+		Unspecified = std::numeric_limits<std::underlying_type_t<decomp_error_t>>::max() /*!< Unspecified Error */
+	};
+
+	/*! \brief Return a human readable string describing the File I/O error.
+
+		This returns a non-localized string describing the meaning of the `file_error_t` if possible.
+
+		For localization, this should be fed into the translation engine for the front-end.
+
+		\param err The error to describe.
+	*/
 	[[nodiscard]]
 	PANKO_API const std::string_view file_error_message(file_error_t err) noexcept;
+
+	/*! \brief Return a human readable string describing the string decoding error.
+
+		This returns a non-localized string describing the meaning of the `strdec_error_t` if possible.
+
+		For localization, this should be fed into the translation engine for the front-end.
+
+		\param err The error to describe.
+	*/
+	[[nodiscard]]
+	PANKO_API const std::string_view strdec_error_message(strdec_error_t err) noexcept;
+
+	/*! \brief Return a human readable string describing the decompression error.
+
+		This returns a non-localized string describing the meaning of the `decomp_error_t` if possible.
+
+		For localization, this should be fed into the translation engine for the front-end.
+
+		\param err The error to describe.
+	*/
+	[[nodiscard]]
+	PANKO_API const std::string_view decomp_error_message(decomp_error_t err) noexcept;
 }
 
 #endif /* PANKO_CORE_ERRCODES_HH */
