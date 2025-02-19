1ee392595cbfa1e1837fdb576c49de561f395999
Aki Van Ness <aki@lethalbit.net>
2025-02-19 05:02:27 -0800

    contrib: scripts: Added `pcap` test file generation

--- contrib/scripts/gen_test_data.sh
+++ contrib/scripts/gen_test_data.sh
@@ -15,5 +15,19 @@ function gen_test0() {
 	zstd -k -19 $PCAPNG_NAME
 }
 
+function gen_test1() {
+	PCAP_NAME="test1.pcap"
+	if [ -e $PCAP_NAME ]; then exit 0; fi
+	# Generate the pcap
+	randpkt -b 128 -c 1 -F pcap -t tcp $PCAP_NAME
+	# Generate the compressed versions
+	bzip2 -zk9 $PCAP_NAME
+	gzip -k9 $PCAP_NAME
+	lz4 -zk -12 $PCAP_NAME
+	lzma -kz9 $PCAP_NAME
+	xz -kz9 $PCAP_NAME
+	zstd -k -19 $PCAP_NAME
+}
 
 gen_test0
+gen_test1
