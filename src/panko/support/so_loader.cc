b3ad1483b3bf79d822d1f42a9a27c24c06292ecc
Aki Van Ness <aki@lethalbit.net>
2025-02-19 02:46:06 -0800

    panko: support: Stubbed out shared object loader file for eventual implementation

---
+++ src/panko/support/so_loader.cc
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* so_loader.cc - Dissector shared object loader */
+
+#include "panko/config.hh"
+#include "panko/internal/defs.hh"
+
+#include "panko/support/so_loader.hh"
+
+namespace Panko::support {
+
+}
---
+++ src/panko/support/so_loader.hh
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* so_loader.hh - Dissector shared object loader */
+
+#pragma once
+#if !defined(PANKO_SUPPORT_SO_LOADER_HH)
+#define PANKO_SUPPORT_SO_LOADER_HH
+
+#include <cstdint>
+#include <vector>
+#include <type_traits>
+
+
+namespace Panko::support {
+
+}
+
+#endif /* PANKO_SUPPORT_SO_LOADER_HH */
